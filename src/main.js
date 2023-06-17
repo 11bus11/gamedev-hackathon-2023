8857be4201474408a88cb8264fe1d3ee9d72f902
sean <seanyoung247@gmail.com>
2023-06-17 10:41:15 +0100

    refactor: Moves scene to external file

--- src/main.js
+++ src/main.js
@@ -1,36 +1,4 @@
-class Example extends Phaser.Scene {
-  constructor() {
-    super();
-  }
-
-  preload() {
-    this.load.setBaseURL("https://labs.phaser.io");
-
-    this.load.image("sky", "assets/skies/space3.png");
-    this.load.image("logo", "assets/sprites/phaser3-logo.png");
-    this.load.image("red", "assets/particles/red.png");
-  }
-
-  create() {
-    this.add.image(400, 300, "sky");
-    this.scale.displaySize.setAspectRatio(1920 / 1080);
-    this.scale.refresh();
-
-    const particles = this.add.particles(0, 0, "red", {
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: "ADD",
-    });
-
-    const logo = this.physics.add.image(400, 100, "logo");
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    particles.startFollow(logo);
-  }
-}
+import { Example } from "./scenes/example";
 
 const config = {
   scale: {
