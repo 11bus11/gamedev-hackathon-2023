c132bb7c124b540500d24abd93b915eecf8b1e89
sean <seanyoung247@gmail.com>
2023-06-18 14:35:42 +0100

    feat: Adds crystals to the map

---
+++ design/maps/test/crystal.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="crystal" tilewidth="16" tileheight="16" tilecount="4" columns="4">
+ <image source="../../../src/assets/items/time-crystal.png" width="64" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="1" width="10" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="1" width="10" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="1" width="10" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.86075" y="0.916452" width="10.1392" height="15.0835"/>
+  </objectgroup>
+ </tile>
+</tileset>
